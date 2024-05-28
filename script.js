74ebf83610ddf66cdf96d29bbdbcc3d8181cc9ff
Amandinha2302 <148460065+Amandinha2302@users.noreply.github.com>
2024-05-28 08:34:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = ducument.querySelector(".caixa-perguntas");
+const caixaAlternativas = docment.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado")
 
+const perguntas = [
+  {
+    enunciado:"Pergunta1";
+    alternativas:[
+      "alternativa1";
+      "alternativa2";
+]
+},
+  {
+    enunciado:"Pergunta2";
+    alternativas:[
+      "alternativa1";
+      "alternativa2";
+]
+},
+  {
+    enunciado:"Pergunta3";
+    alternativas:[
+      "alternativa1";
+      "alternativa2";
+]
+},
+  {
+    enunciado:"Pergunta4";
+    alternativas:[
+      "alternativa1";
+      "alternativa2";
+]
+},
+  {
+    enunciado:"Pergunta5";
+    alternativas:[
+      "alternativa1";
+      "alternativa2";
+]
+},
+];
