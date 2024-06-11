759e2a950da6fa780685ec32abccd8d2a10b33e3
Amandinha2302 <148460065+Amandinha2302@users.noreply.github.com>
2024-06-11 08:48:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ const perguntas = [
   {
     enunciado:"Você gostaria de ir á Italia?";
     alternativas:[
-      "Sim";
-      "Não";
-]
-},
-  {
-    enunciado:"Você já viajou?";
+      {
+    texto:"sim",
+      afirmação:"afirmação",
+      }
+      {
+      texto:"não",
+      afirmação:"afirmação"
+      }
+      ]
+}
+{
+     enunciado:"Você já viajou?";
     alternativas:[
       "Sim";
       "Não";
@@ -41,3 +47,22 @@ const perguntas = [
 ]
 },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
