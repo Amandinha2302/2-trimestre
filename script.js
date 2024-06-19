667ca320bfd587fb5ec2ef7f6f87776b6c5d034b
Amandinha2302 <148460065+Amandinha2302@users.noreply.github.com>
2024-06-19 08:47:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -53,8 +53,13 @@ let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta() {
+  if(atual >= perguntas.lenght){
+    mostraResultado();
+    return;
+}
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
@@ -68,8 +73,11 @@ function mostraAlternativas() {
 }
 function repostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmacoes;
-    historiaFinal = afirmacoes;
+    historiaFinal += afirmacoes + "";
     atual++;
     mostraPergunta();
+}
+function mostraResposta(){
+  caixaPerguntas.textContent = "";
 }
 mostraPergunta();
